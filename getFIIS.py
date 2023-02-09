dbb1dc272d0b120c563bb5b1e9bf049bbe952be5
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2023-02-09 22:23:15 +1000

    fixes

--- getFIIS.py
+++ getFIIS.py
@@ -1,41 +1,47 @@
-from urllib.request import urlopen, Request
+import requests
 from bs4 import BeautifulSoup
-import re
-
-##carrega os papeis de stocksList.csv
-stocksList = open('ifix.csv', 'r')
-papeis = stocksList.read().split(';')
-##papeis = ["VRTA11"]
-stocksList.close()
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
 def getpapel(papel_):
-    try:
-                    url = "https://fiis.com.br/"                    
-                    r = Request(url+papel_, headers={'User-Agent': 'Mozilla/5.0'})
-                    html = urlopen(r).read()
-                    ##print(html)
-                    soup = BeautifulSoup(html, 'html.parser')                    
-                    result = soup.find_all("div", class_="indicators__box")
-                    ##print(len(result))
-                    alldata = []
-                    
+    try:                                    
+        r=requests.get("https://fiis.com.br/"+papel_,headers=headers)                    
+        soup = BeautifulSoup(r.content, "html.parser")
+        linha_tabela = soup.find_all("div","yieldChart__table__bloco")
+        print("Encontramos "+str(len(linha_tabela))+" entradas de dividendos para o FII " + papel_)
+        for linha in linha_tabela:    
+            valores = linha.find_all("div")
+            linha=""
+            linha+=papel_+" | "
+            for valor in valores:
+                linha+=(valor.text.replace('\r', '').replace('\n', '').replace(' ', ''))+" | "
+            print(linha)
 
-                    print(papel+" "+re.sub('\s+',' ',result[0].text.strip()))
-                    ##for row in result:
-                        ##print(row.text)                        
-                                              
-                    papeis.remove(papel_)
-                    
+        print("Removendo papel: "+str(papel_))                                                           
+        ##papeis.remove(papel_)
+        ##print("Faltam: "+str(len(papeis)))                    
                     
     except Exception as e:
-        print("#ERRO: ",papel," ", str(e))
-        papeis.remove(papel_)   
+        print("#ERRO:",papel_, str(e))
+        ##papeis.remove(papel_)   
 
-while len(papeis)>0:
-    for papel in papeis:
-        ##print("Procurando papel "+papel)
-        getpapel(papel)
-            
+papeis = []
+
+with open("Allfiis.csv", "r") as file:
+    for line in file:
+        ##print(line)
+        line_data = line.strip().split(";")
+        for tick in line_data:
+              papeis.append(tick)
+
+print(papeis)
+print("Procurando dados de #"+str(len(papeis)))
+
+for papel in papeis:
+      getpapel(papel)
 
 
+
+
+
+        
